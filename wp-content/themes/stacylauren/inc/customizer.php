e91f02109db7c5c6ae6317440212ea45eb1cfc67
Stacy Moorhead <spezzola@uarts.edu>
2018-03-16 16:17:15 +0000

    installed wordpress and set up dev environment

---
+++ wp-content/themes/stacylauren/inc/customizer.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Stacy Lauren Theme Customizer
+ *
+ * @package Stacy_Lauren
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function stacylauren_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial( 'blogname', array(
+			'selector'        => '.site-title a',
+			'render_callback' => 'stacylauren_customize_partial_blogname',
+		) );
+		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+			'selector'        => '.site-description',
+			'render_callback' => 'stacylauren_customize_partial_blogdescription',
+		) );
+	}
+}
+add_action( 'customize_register', 'stacylauren_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function stacylauren_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function stacylauren_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function stacylauren_customize_preview_js() {
+	wp_enqueue_script( 'stacylauren-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'stacylauren_customize_preview_js' );

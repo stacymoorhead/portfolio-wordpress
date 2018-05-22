a8193e89fb05729fc735d16ac2db698ee7f877cb
Stacy Moorhead <spezzola@uarts.edu>
2018-05-22 18:09:53 +0000

    added recaptcha js and css

--- wp-content/themes/stacylauren/functions.php
+++ wp-content/themes/stacylauren/functions.php
@@ -265,6 +265,8 @@ function stacylauren_scripts() {
 	//wp_enqueue_script('stacylauren-toucheffects');
 
 	wp_enqueue_script( 'stacylauren-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	
+	wp_enqueue_script('stacylauren-recaptcha', 'https://www.google.com/recaptcha/api.js', array (), '', true);
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );

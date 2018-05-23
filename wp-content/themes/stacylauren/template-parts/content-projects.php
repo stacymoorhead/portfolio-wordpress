11cef035a78e4b5ae5fb6f2e12256cdb78fb1b73
Stacy Moorhead <spezzola@uarts.edu>
2018-05-23 15:52:41 +0000

    fixed issue with content not displaying on single

--- wp-content/themes/stacylauren/template-parts/content-projects.php
+++ wp-content/themes/stacylauren/template-parts/content-projects.php
@@ -16,12 +16,13 @@
 		} endif; */
 		
 		stacylauren_post_thumbnail(); 
-		/*the_content( sprintf(
+		if (is_single()) : {
+		the_content( sprintf(
 			/* translators: %s: Name of current post. */
-			/*wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'stacylauren' ), array( 'span' => array( 'class' => array() ) ) ),
+			wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'stacylauren' ), array( 'span' => array( 'class' => array() ) ) ),
 			the_title( '<span class="screen-reader-text">"', '"</span>', false )
-		) );*/
-
+		) );
+		} endif;
 		wp_link_pages( array(
 			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'stacylauren' ),
 			'after'  => '</div>',
